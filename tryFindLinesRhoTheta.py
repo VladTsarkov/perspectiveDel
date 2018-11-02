d196f88ad09efa866b65fae7595b19c4ed571db1
VladTsarkov <43319801+VladTsarkov@users.noreply.github.com>
2018-11-03 01:39:48 +0500

    Add files via upload

--- tryFindLinesRhoTheta.py
+++ tryFindLinesRhoTheta.py
@@ -2,7 +2,14 @@ import cv2
 import numpy as np
 import time
 
+def intersectionX(x1,x2,y1,y2,x3,x4,y3,y4):
+	return int(((x1*y2-y1*x2)*(x3-x4)-(x1-x2)*(x3*y4-y3*x4))\
+		/((x1-x2)*(y3-y4)-(y1-y2)*(x3-x4)))
+def intersectionY(x1,x2,y1,y2,x3,x4,y3,y4):
+	return int(((x1*y2-y1*x2)*(y3-y4)-(y1-y2)*(x3*y4-y3*x4))\
+		/((x1-x2)*(y3-y4)-(y1-y2)*(x3-x4)))
 img = cv2.imread('test.jpg')
+shape = img.shape
 blur = cv2.blur(img,(3,3))
 gray = cv2.cvtColor(blur,cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray,200,300,apertureSize = 3)
@@ -41,7 +48,7 @@ for key1,line2 in enumerate(lines2):
 	#for key,line in enumerate(lines2):
 	#	if line2[0][0] != line[0][0] and line2[0][1] != line[0][1] and np.absolute(line2[0][0]-line[0][0])<=100 and np.absolute(line2[0][1]-line[0][1])<=0.1:
 	#		del(lines2[key])
-print(temp)
+#print(temp)
 #print(lines)
 for line in temp:
 	a = np.cos(line[1])
@@ -85,8 +92,12 @@ for line in temp:
 		y0 = b*line1[0]
 		if  line1[0] != line[0] and np.absolute(x10-x0)<=20 and np.absolute(y10-y0)<=20:
 			del(temp[key])
-ktemp = -10000000
-ktemp1 = -10000000
+ktemp = 10000000
+ktemp1 = 10000000
+ktemp3 = -10000000
+ktemp4 = -10000000
+xMin = 10000
+xMax = 0
 for line in temp:
 	a = np.cos(line[1])
 	b = np.sin(line[1])
@@ -102,10 +113,60 @@ for line in temp:
 	b=(x1*y2-x2*y1)/(x1-x2)
 	ugol=np.arctan(k)*180/np.pi
 	print("ugol ",ugol)
+	if ugol>=80 and ugol<=100 or ugol>=-100 and ugol<=-80:
+		if x1 < xMin:
+			xMin = x1
+			ltempx1 = x1
+			ltempx2 = x2
+			ltempy1 = y1
+			ltempy2 = y2
+		if x1 > xMax:
+			xMax = x1
+			rtempx1 = x1
+			rtempx2 = x2
+			rtempy1 = y1
+			rtempy2 = y2
+		#cv2.line(img4,(x1,y1),(x2,y2),(0,0,0),2)
 	if ugol>=20 and ugol<=60:
 		cv2.line(img4,(x1,y1),(x2,y2),(255,0,0),2)
+		if b<ktemp:
+			ktemp = b
+			tempx1 = x1
+			tempx2 = x2
+			tempy1 = y1
+			tempy2 = y2
+		if b>ktemp3:
+			ktemp3 = b
+			btempx1 = x1
+			btempx2 = x2
+			btempy1 = y1
+			btempy2 = y2
 	if ugol>=-60 and ugol<=-20:
 		cv2.line(img4,(x1,y1),(x2,y2),(0,255,0),2)
+		if b<ktemp1:
+			ktemp1 = b
+			tempx11 = x1
+			tempx22 = x2
+			tempy11 = y1
+			tempy22 = y2
+		if b>ktemp4:
+			ktemp4 = b
+			btempx11 = x1
+			btempx22 = x2
+			btempy11 = y1
+			btempy22 = y2
+cv2.line(img4,(tempx1,tempy1),(tempx2,tempy2),(255,255,0),2)
+cv2.line(img4,(tempx11,tempy11),(tempx22,tempy22),(255,0,255),2)
+
+cv2.line(img4,(btempx1,btempy1),(btempx2,btempy2),(0,0,255),2)
+cv2.line(img4,(btempx11,btempy11),(btempx22,btempy22),(0,0,200),2)
+x3 = intersectionX(tempx1,tempx2,tempy1,tempy2,tempx11,tempx22,tempy11,tempy22)
+y3 = intersectionY(tempx1,tempx2,tempy1,tempy2,tempx11,tempx22,tempy11,tempy22)
+
+cv2.line(img4,(x3,y3),(x3,shape[1]),(255,255,255),2) # middle
+cv2.line(img4,(ltempx1,ltempy1),(ltempx2,ltempy2),(0,0,0),2)
+cv2.line(img4,(rtempx1,rtempy1),(rtempx2,rtempy2),(0,0,0),2)
+
 cv2.imwrite('test_2_4.jpg',img4)
 for line in temp:
 	f+=1
