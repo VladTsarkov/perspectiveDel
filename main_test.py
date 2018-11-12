7614f4f3c75f80c762f2354a56a02a0ebe2dae2c
VladTsarkov <43319801+VladTsarkov@users.noreply.github.com>
2018-11-13 00:34:09 +0500

    Add files via upload

--- main_test.py
+++ main_test.py
@@ -1,18 +1,31 @@
 import cv2
 import numpy as np
-def Transform(img, x1,y1,x2,y2,x3,y3,x4,y4):
+def Transform(img,tempt, x1,y1,x2,y2,x3,y3,x4,y4):
     src = np.array([
-    [x1,x2],
+    [x1,y1],
     [x2,y2],
     [x3,y3],
     [x4,y4]], dtype = "float32")
     (tl,tr,br,bl)=src
+    print(src)
     widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
     widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
     maxWidth = max(int(widthA), int(widthB))
     heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
     heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
     maxHeight = max(int(heightA), int(heightB))
+
+    """
+
+    heightA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    heightB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+    widthA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    widthB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+    """
+    print("THEFL? ",tl[0],bl[0],tl[1],bl[1])
+    print("THEFR? ",tr[0],br[0],tr[1],br[1])
     dst = np.array([
     [0, 0],
     [maxWidth - 1, 0],
@@ -20,4 +33,4 @@ def Transform(img, x1,y1,x2,y2,x3,y3,x4,y4):
     [0, maxHeight - 1]], dtype = "float32")
     M = cv2.getPerspectiveTransform(src, dst)
     warped = cv2.warpPerspective(img, M, (maxWidth, maxHeight))
-    cv2.imwrite('test_warp.jpg',warped)
+    cv2.imwrite(tempt,warped)
--- tryFindLinesRhoTheta.py
+++ tryFindLinesRhoTheta.py
@@ -17,8 +17,17 @@ edges = cv2.Canny(gray,200,300,apertureSize = 3)
 cv2.imwrite('test_gray.jpg',edges)
 minLineLength = 500
 maxLineGap = 20
-lines = cv2.HoughLinesP(edges,1,np.pi/180,50,minLineLength,maxLineGap)
-lines2 = cv2.HoughLines(edges,1,np.pi/180,70)
+
+
+GoodAtLines=True
+Thresh = 70
+while (GoodAtLines):
+	lines2 = cv2.HoughLines(edges,1,np.pi/180,Thresh)
+	if len(lines2)>2000:
+		Thresh+=10
+	else:
+		GoodAtLines = False
+
 #print(lines2)
 img1 = img.copy()
 img2 = img.copy()
@@ -32,8 +41,6 @@ for key1,line2 in enumerate(lines2):
 	j+=1
 	a = np.cos(line2[0][1])
 	b = np.sin(line2[0][1])
-	#print("ab", line2[0][1] , line2[0][0] )
-	#time.sleep(3)
 	x0 = a*line2[0][0]
 	y0 = b*line2[0][0]
 	x1 = int(x0 + 1000*(-b))
@@ -42,11 +49,7 @@ for key1,line2 in enumerate(lines2):
 	y2 = int(y0 - 1000*(a))
 	theta = line2[0][1]*90
 	temp.append([line2[0][0],line2[0][1]])
-	#for key,line in enumerate(lines2):
-	#	if line2[0][0] != line[0][0] and line2[0][1] != line[0][1] and np.absolute(line2[0][0]-line[0][0])<=100 and np.absolute(line2[0][1]-line[0][1])<=0.1:
-	#		del(lines2[key])
-#print(temp)
-#print(lines)
+
 for line in temp:
 	a = np.cos(line[1])
 	b = np.sin(line[1])
@@ -59,8 +62,8 @@ for line in temp:
 	cv2.line(img3,(x1,y1),(x2,y2),(0,0,255),2)
 cv2.imwrite('test_2_3.jpg',img3)
 for line in temp:
-	for key,line1 in enumerate(temp):
-		if line1[0] != line[0] and line1[1] != line[1] and np.absolute(line1[0]-line[0])<=160 and np.absolute(line1[1]-line[1])<=0.1:
+	for key,line1 in enumerate(temp): #line1 - line = bilo 160
+		if line1[0] != line[0] and line1[1] != line[1] and np.absolute(line1[0]-line[0])<=50 and np.absolute(line1[1]-line[1])<=0.1:
 			del(temp[key])
 
 for line in temp:
@@ -80,7 +83,7 @@ for line in temp:
 	b1 = np.sin(line[1])
 	x10 = a1*line[0]
 	y10 = b1*line[0]
-	print("XY10 ",x10,y10)
+	#print("XY10 ",x10,y10)
 
 	for key,line1 in enumerate(temp):
 		a = np.cos(line1[1])
@@ -110,6 +113,7 @@ for line in temp:
 	b=(x1*y2-x2*y1)/(x1-x2)
 	ugol=np.arctan(k)*180/np.pi
 	print("ugol ",ugol)
+	"""
 	if ugol>=80 and ugol<=100 or ugol>=-100 and ugol<=-80:
 		if x1 < xMin:
 			xMin = x1
@@ -124,8 +128,9 @@ for line in temp:
 			rtempy1 = y1
 			rtempy2 = y2
 		#cv2.line(img4,(x1,y1),(x2,y2),(0,0,0),2)
-	if ugol>=20 and ugol<=60:
-		cv2.line(img4,(x1,y1),(x2,y2),(255,0,0),2)
+	"""
+	if ugol>=-6 and ugol<=60:
+		#cv2.line(img4,(x1,y1),(x2,y2),(0,0,0),2)
 		if b<ktemp:
 			ktemp = b
 			tempx1 = x1
@@ -138,8 +143,8 @@ for line in temp:
 			btempx2 = x2
 			btempy1 = y1
 			btempy2 = y2
-	if ugol>=-60 and ugol<=0:
-		cv2.line(img4,(x1,y1),(x2,y2),(0,255,0),2)
+	if ugol>=-60 and ugol<=6:
+		#cv2.line(img4,(x1,y1),(x2,y2),(0,255,0),2)
 		if b<ktemp1:
 			ktemp1 = b
 			tempx11 = x1
@@ -156,7 +161,13 @@ cv2.line(img4,(tempx1,tempy1),(tempx2,tempy2),(255,255,0),2) #TL
 cv2.line(img4,(tempx11,tempy11),(tempx22,tempy22),(255,0,255),2) #TR
 
 cv2.line(img4,(btempx1,btempy1),(btempx2,btempy2),(0,0,255),2) #BR
-cv2.line(img4,(btempx11,btempy11),(btempx22,btempy22),(0,0,200),2) #BL
+cv2.line(img4,(btempx11,btempy11),(btempx22,btempy22),(0,244,200),2) #BL
+
+#if ltempx1>200:
+#	ltempx1,ltempx2,ltempy1,ltempy2 = (0, 0, 0, shape[1])
+ltempx1,ltempx2,ltempy1,ltempy2 = (0, 0, 0, shape[1])
+rtempx1,rtempx2,rtempy1,rtempy2 = (shape[1], shape[1], 0, shape[0])
+print("shapes", shape[0],shape[1])
 
 x2 = intersectionX(tempx1,tempx2,tempy1,tempy2,tempx11,tempx22,tempy11,tempy22)#MiddleTopPoint
 y2 = intersectionY(tempx1,tempx2,tempy1,tempy2,tempx11,tempx22,tempy11,tempy22)
@@ -169,21 +180,33 @@ y3 = intersectionY(btempx11,btempx22,btempy11,btempy22,x2,x2,y2,shape[1])
 
 x4 = intersectionX(btempx11,btempx22,btempy11,btempy22,ltempx1,ltempx2,ltempy1,ltempy2)#BottomLeftPoint
 y4 = intersectionY(btempx11,btempx22,btempy11,btempy22,ltempx1,ltempx2,ltempy1,ltempy2)
-main_test.Transform(img, x1,y1,x2,y2,x3,y3,x4,y4)
+tempt = 'test_warp.jpg'
+main_test.Transform(img,tempt, x1,y1,x2,y2,x3,y3,x4,y4)
 cv2.line(img4,(x2,y2),(x2,shape[1]),(255,255,255),2) # middle
-cv2.line(img4,(ltempx1,ltempy1),(ltempx2,ltempy2),(0,0,0),2)
-cv2.line(img4,(rtempx1,rtempy1),(rtempx2,rtempy2),(0,0,0),2)
+print(x2,y2)
+tempt = 'test_warp2.jpg'
+x1, y1 = x2, y2
+print(tempx11,tempx22,tempy11,tempy22,rtempx1,rtempx2,rtempy1,rtempy2)
+x2 = intersectionX(tempx11,tempx22,tempy11,tempy22,rtempx1,rtempx2,rtempy1,rtempy2)#TopRightPoint
+y2 = intersectionY(tempx11,tempx22,tempy11,tempy22,rtempx1,rtempx2,rtempy1,rtempy2)
+
+x3 = intersectionX(btempx1,btempx2,btempy1,btempy2,rtempx1,rtempx2,rtempy1,rtempy2)#BottomRightPoint
+y3 = intersectionY(btempx1,btempx2,btempy1,btempy2,rtempx1,rtempx2,rtempy1,rtempy2)
+
+x4 = intersectionX(btempx1,btempx2,btempy1,btempy2,x1,x1,y1,shape[1])#BottomLeftPoint
+y4 = intersectionY(btempx1,btempx2,btempy1,btempy2,x1,x1,y1,shape[1])
+print(x1,y1,x2,y2,x3,y3,x4,y4)
+main_test.Transform(img,tempt, x1,y1,x2,y2,x3,y3,x4,y4)
+#cv2.line(img4,(ltempx1,ltempy1),(ltempx2,ltempy2),(0,20,0),2) #LeftVert
+#cv2.line(img4,(rtempx1,rtempy1),(rtempx2,rtempy2),(0,0,0),2) # RightVert
 
 cv2.imwrite('test_2_4.jpg',img4)
 for line in temp:
 	f+=1
 	a = np.cos(line[1])
 	b = np.sin(line[1])
-	#print("ab", line2[0][1] , line2[0][0] )
-	#time.sleep(3)
 	x0 = a*line[0]
 	y0 = b*line[0]
-	print("XY",x0,y0)
 	x1 = int(x0 + 1000*(-b))
 	y1 = int(y0 + 1000*(a))
 	x2 = int(x0 - 1000*(-b))
@@ -192,12 +215,6 @@ for line in temp:
 cv2.imwrite('test_2.jpg',img1)
 i=0
 
-
-for line in lines:
-	i+=1
-	cv2.line(img,(line[0][0],line[0][1]),(line[0][2],line[0][3]),(0,255,0),2)
-cv2.imwrite('test_1.jpg',img)
-cv2.imwrite('test1.jpg',blur)
-print("lines count: ",i)
 print("lines2WOD count: ",j)
 print("lines2WD count: ",f)
+#cv2.imwrite('test1.jpg',blur)
